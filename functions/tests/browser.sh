e859b39988c5cc036b6a078b697e617496cc0275
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-16 21:51:14 +0000

    Improved browser test output

--- functions/tests/browser.sh
+++ functions/tests/browser.sh
@@ -6,6 +6,7 @@ browserTest() {
     rm -rf "${current_dir}/tests/browser/tmp"
     rm -rf "${current_dir}/tests/browser/tmpfile:"
     mkdir -p "${current_dir}/tests/browser/tmp"
+
     if [[ "$first_half" == true ]]; then
         echo "$(tput setaf 4)-- Launching $browsers browsers on cores ( 0-$((half_cores - 1)) )$(tput sgr0)" | tee -a log.txt
     elif [[ "$second_half" == true ]]; then
@@ -13,6 +14,8 @@ browserTest() {
     elif
         echo "$(tput setaf 4)-- Launching $browsers browsers on all cores$(tput sgr0)" | tee -a log.txt
     fi
+    echo "$(tput setaf 3)[DEBUG] Appimage $current_dir/tests/browser/$chromium_appimage"
+    echo "$(tput setaf 3)[DEBUG] --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp"
     for ((i = 0; i < browsers; i++)); do
         random_page=$((RANDOM % 6 + 1))
         file_path="file://$current_dir/tests/browser/pages/$random_page.html"
@@ -23,14 +26,14 @@ browserTest() {
             if [[ "$first_half" == true ]]; then
                 # first half of cpu
                 echo "$(tput setaf 4)-- Launching $browsers browsers on cores ( 0-$((half_cores - 1)) )$(tput sgr0)" | tee -a log.txt
-                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list 0-$((half_cores - 1)) ./tests/browser/$chromium_appimage $file_path$(tput sgr0)" | tee -a log.txt
-                taskset --cpu-list 0-$((half_cores - 1)) ./tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
+                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list 0-$((half_cores - 1)) $file_path$(tput sgr0)" | tee -a log.txt
+                taskset --cpu-list 0-$((half_cores - 1)) $current_dir/tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
             fi
             if [[ "$second_half" == true ]]; then
                 # second half of cpu
                 echo "$(tput setaf 4)-- Launching $browsers browsers on cores ( $half_cores-$((num_cores - 1)) )$(tput sgr0)" | tee -a log.txt
-                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list $half_cores-$((num_cores - 1)) ./tests/browser/$chromium_appimage $file_path$(tput sgr0)" | tee -a log.txt
-                taskset --cpu-list $half_cores-$((num_cores - 1)) ./tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
+                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list $half_cores-$((num_cores - 1)) $file_path$(tput sgr0)" | tee -a log.txt
+                taskset --cpu-list $half_cores-$((num_cores - 1)) $current_dir/tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
             fi
             sleep $rest_time
         else
@@ -39,15 +42,15 @@ browserTest() {
             half_cores=$((num_cores / 2))
             if (( browsers > 1 )); then
                 if (( (i + 1) % 2 == 0 )); then
-                    echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list 0-$((half_cores - 1)) ./tests/browser/$chromium_appimage $file_path$(tput sgr0)" | tee -a log.txt
-                    taskset --cpu-list 0-$((half_cores - 1)) ./tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
+                    echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list 0-$((half_cores - 1)) $file_path$(tput sgr0)" | tee -a log.txt
+                    taskset --cpu-list 0-$((half_cores - 1)) $current_dir/tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
                 else
-                    echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list $half_cores-$((num_cores - 1)) ./tests/browser/$chromium_appimage $file_path$(tput sgr0)" | tee -a log.txt
-                    taskset --cpu-list $half_cores-$((num_cores - 1)) ./tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
+                    echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list $half_cores-$((num_cores - 1)) $file_path$(tput sgr0)" | tee -a log.txt
+                    taskset --cpu-list $half_cores-$((num_cores - 1)) $current_dir/tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
                 fi
             else
-                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list 0-$((num_cores - 1)) ./tests/browser/$chromium_appimage $file_path$(tput sgr0)" | tee -a log.txt
-                taskset --cpu-list 0-$((num_cores - 1)) ./tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
+                echo "$(tput setaf 3)[DEBUG] taskset --cpu-list 0-$((num_cores - 1)) $file_path$(tput sgr0)" | tee -a log.txt
+                taskset --cpu-list 0-$((num_cores - 1)) $current_dir/tests/browser/$chromium_appimage --new-window --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito --user-data-dir=$current_dir/tests/browser/tmp "$file_path" > /dev/null 2>&1 &
             fi
             sleep $rest_time
         fi
