615682da9757cfe5c278361bc0e047f6cd5e3b64
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-16 21:40:03 +0000

    update settings with chrome

--- settings.sh
+++ settings.sh
@@ -6,11 +6,17 @@ mixed=("prime" "matrixprod" "fft" "loop")
 heavy=("ackermann" "factorial")
 rapid="bitops"
 
-# stress test times - # 280s - 560s - 9m (74m (1.25h) 8 cores) (149m (2.5h) 16 threads) - 9800X3d example times
-light_time="5s" # 40s - 80s
-medium_time="15s" # 120s - 240s
-heavy_time="30s" # 120s - 240s
-all_core_time=5
-rapid_tests=1
-rapid_time="1s"
+# stress test times
+# Defaults: 336s (per core or thread) - (45m for 8 cores) - (90m for 16 threads)
+light_time="5s" # 32s - (64s browsers)
+medium_time="15s" # 72s - (144s browsers)
+heavy_time="30s" # 63s - (126s browsers)
+all_core_time=15 # 120s
+rapid_tests=5 # 80s (8 cores) - 160s (16 cores)
+rapid_time="2s"
 rest_time=3
+
+# browser config
+chromium_version="144.0.7559.59-1"
+chromium_appimage="ungoogled-chromium-$chromium_version-x86_64.AppImage"
+chromium_domain="https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases/download"
