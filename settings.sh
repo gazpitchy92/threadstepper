0bf470050cdd0babfc9f90ea023ebac3fc5fa013
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-17 01:50:56 +0000

    comments on settings

--- settings.sh
+++ settings.sh
@@ -26,9 +26,11 @@ rapid_tests=5
 rapid_time="1s"
 rest_time=2
 
-# browser config
+# chromium browser test config
+# re-install dependancies if changed
 chromium_version="144.0.7559.59-1"
 chromium_appimage="ungoogled-chromium-$chromium_version-x86_64.AppImage"
 chromium_domain="https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/releases/download"
 chromium_flags="--new-window --ozone-platform=x11 --no-sandbox --disable-gpu-sandbox --disable-dev-shm-usage --disable-gpu-compositing --disable-accelerated-2d-canvas --disable-accelerated-video-decode --disable-accelerated-video-encode --disable-webgl2 --num-raster-threads=1 --incognito"
+
 
