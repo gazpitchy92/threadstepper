f2090c8724525cbd2e107b8226cc574a84e55940
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-19 02:04:58 +0000

    new all-core methodology

--- functions/tests/allcore.sh
+++ functions/tests/allcore.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
-
-# full cpu 7zip stress
 allCoreTest() {
-    echo "$(tput setaf 4)Running 7z warmup on all cores $(tput sgr0)" | tee -a $output_log_file
-    for i in $(seq 1 $all_core_time); do
-        # increase test time by 1s until $all_core_time
-        echo "$(tput setaf 2)Running 7z for $i seconds $(tput sgr0)" | tee -a $output_log_file
-        7z b > /dev/null 2>&1 &
-        sleep "$i"
-        pkill 7z
+    echo "$(tput setaf 4)Running cumulative 7z stress test on all cores$(tput sgr0)" | tee -a "$output_log_file"
+    total_cores=$(nproc)
+    
+    for core in $(seq 0 $((total_cores - 1))); do
+        cores_list=$(seq -s, 0 "$core")
+        echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
+        taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        sleep "$all_core_time"
         check_errors
-        sleep $rest_time
+        pkill -9 7z > /dev/null 2>&1
+    done
+    
+    for core in $(seq $((total_cores - 1)) -1 0); do
+        cores_list=$(seq -s, 0 "$core")
+        echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
+        taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        sleep "$all_core_time"
+        check_errors
+        pkill -9 7z > /dev/null 2>&1
+    done
+    
+    for core in $(seq $((total_cores - 1)) -1 0); do
+        cores_list=$(seq -s, "$core" $((total_cores - 1)))
+        echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
+        taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        sleep "$all_core_time"
+        check_errors
+        pkill -9 7z > /dev/null 2>&1
+    done
+    
+    for core in $(seq 0 $((total_cores - 1))); do
+        cores_list=$(seq -s, "$core" $((total_cores - 1)))
+        echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
+        taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        sleep "$all_core_time"
+        check_errors
+        pkill -9 7z > /dev/null 2>&1
     done
 }
