8bb8ab1375c00f9b81be839fc068c5dba08e313a
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-19 02:13:10 +0000

    remove last allcore test

--- functions/tests/allcore.sh
+++ functions/tests/allcore.sh
@@ -14,7 +14,7 @@ allCoreTest() {
         pkill -9 7z > /dev/null 2>&1
     done
     
-    for core in $(seq $((total_cores - 1)) -1 0); do
+    for core in $(seq $((total_cores - 2)) -1 0); do
         cores_list=$(seq -s, 0 "$core")
         echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
         taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
@@ -33,14 +33,5 @@ allCoreTest() {
         check_errors
         pkill -9 7z > /dev/null 2>&1
     done
-    
-    for core in $(seq 0 $((total_cores - 1))); do
-        cores_list=$(seq -s, "$core" $((total_cores - 1)))
-        echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
-        taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
-        disown
-        sleep "$all_core_time"
-        check_errors
-        pkill -9 7z > /dev/null 2>&1
-    done
+
 }
