870cc0d61bb82b8adcec3485e3b75f1ec2bc63c3
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-01-19 02:07:54 +0000

    fix allcore disown output

--- functions/tests/allcore.sh
+++ functions/tests/allcore.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 allCoreTest() {
-    echo "$(tput setaf 4)Running cumulative 7z stress test on all cores$(tput sgr0)" | tee -a "$output_log_file"
+    echo "$(tput setaf 4)Running cumulative all core test$(tput sgr0)" | tee -a "$output_log_file"
     total_cores=$(nproc)
     
     for core in $(seq 0 $((total_cores - 1))); do
         cores_list=$(seq -s, 0 "$core")
         echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
         taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        disown
         sleep "$all_core_time"
         check_errors
         pkill -9 7z > /dev/null 2>&1
@@ -16,6 +18,7 @@ allCoreTest() {
         cores_list=$(seq -s, 0 "$core")
         echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
         taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        disown
         sleep "$all_core_time"
         check_errors
         pkill -9 7z > /dev/null 2>&1
@@ -25,6 +28,7 @@ allCoreTest() {
         cores_list=$(seq -s, "$core" $((total_cores - 1)))
         echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
         taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        disown
         sleep "$all_core_time"
         check_errors
         pkill -9 7z > /dev/null 2>&1
@@ -34,6 +38,7 @@ allCoreTest() {
         cores_list=$(seq -s, "$core" $((total_cores - 1)))
         echo "$(tput setaf 2)Stressing cores $cores_list for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
         taskset -c "$cores_list" 7z b > /dev/null 2>&1 &
+        disown
         sleep "$all_core_time"
         check_errors
         pkill -9 7z > /dev/null 2>&1
