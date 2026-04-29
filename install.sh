f4cb7227ac416745fe89974c4d30f94ea62d8863
Jamie Johnson <jamiejj@gmail.com>
2026-04-29 16:03:52 -0700

    Add and configure python virtual environment, requirements. Remove runtime temporaries from git

--- install.sh
+++ install.sh
@@ -30,6 +30,11 @@ else
     install_failed
 fi
 
+# Setup virtual environment and install requirements
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 echo "Dependencies have been installed"
 
 APPIMAGE_URL="$chromium_domain/$chromium_version/$chromium_appimage"
