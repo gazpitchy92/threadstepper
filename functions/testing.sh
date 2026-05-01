9f3bddb9e7405fa84b930daff33bd277fafe859d
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-02 00:49:55 +0100

    add new logging bash

--- functions/testing.sh
+++ functions/testing.sh
@@ -13,3 +13,13 @@ runTests(){
         check_errors
     fi 
 }
+
+# Updates progress file for UI
+update_progress(){ 
+    local test=$1
+    local test_loop=$2
+    local test_outof=$3
+    local all_loop=$4
+    local all_outof=$5
+    echo "${test} ${test_loop}/${test_outof} , ${all_loop}/${all_outof}" > $output_progress_file
+}
