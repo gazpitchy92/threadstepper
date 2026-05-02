a02bc4a152c4240c227c00128c9defebf879682e
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-02 02:25:18 +0100

    2.9 update

--- start.py
+++ start.py
@@ -24,7 +24,7 @@ class StressTestGUI:
         
         self.root = root
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.root.title("Thread Stepper (2.8)")
+        self.root.title("Thread Stepper (2.9)")
         self.root.geometry("800x1060")
         
         self.process = None
