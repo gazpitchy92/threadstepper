9c4fe3e458724909a6dd488da3722fe32f7c456e
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-02 06:39:47 +0100

    correct browser test logs logo

--- ui/logs.py
+++ ui/logs.py
@@ -184,9 +184,9 @@ def update_current_test(self):
                     fg="#28a745",
                     bg="#d4edda"
                 )
-            elif test_name == "🌐 Browser Tests":
+            elif test_name == "Browser Tests":
                 self.clock_label_bottom.config(
-                    text="🎯 " + test_name + "\n" + progress,
+                    text="🌐 " + test_name + "\n" + progress,
                     fg="#28a745",
                     bg="#d4edda"
                 )
