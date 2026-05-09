5469b559b069fbea6bbdccaa31cac7080cdc0f05
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-09 23:36:12 +0100

    add low meidum and high loads to all core testing

--- functions/tests/allcore.sh
+++ functions/tests/allcore.sh
@@ -1,6 +1,20 @@
 allCoreTest() {
-    echo "$(tput setaf 4)Running cumulative all core test$(tput sgr0)" | tee -a "$output_log_file"
-    total_cores=$(nproc)
+
+    local total_threads
+    total_threads=$(nproc)
+    local physical_cores=$((total_threads / 2))
+
+    local topo=$cpu_topology
+    [[ "$topo" == "0" ]] && topo=2
+
+    declare -a core_pairs
+    for (( i=0; i<physical_cores; i++ )); do
+        if [[ "$topo" == "1" ]]; then
+            core_pairs+=("$i,$((i + physical_cores))")
+        else
+            core_pairs+=("$((i * 2)),$((i * 2 + 1))")
+        fi
+    done
 
     is_blacklisted() {
         local core=$1
@@ -12,48 +26,101 @@ allCoreTest() {
     }
 
     filter_cores() {
-        local input=$1
         local filtered=()
-        IFS=',' read -ra cores <<< "$input"
+        IFS=',' read -ra cores <<< "$1"
         for core in "${cores[@]}"; do
             is_blacklisted "$core" || filtered+=("$core")
         done
         echo "${filtered[*]}" | tr ' ' ','
     }
 
-    for core in $(seq 0 $((total_cores - 1))); do
-        cores_list=$(seq -s, 0 "$core")
-        cores_list=$(filter_cores "$cores_list")
-        [[ -z "$cores_list" ]] && echo "$(tput setaf 0)Skipping thread(s) $cores_list as disabled$(tput sgr0)" | tee -a "$output_log_file" && continue
-        echo "$(tput setaf 2)Stressing thread(s) [$cores_list] for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
-        update_threads "$cores_list"
-        start_stressor "$cores_list"
-        sleep "$all_core_time"
-        stop_stressor
-        check_errors
+    declare -a stress_sets
+    local cumulative=""
+    for (( i=0; i<physical_cores; i++ )); do
+        if [[ -z "$cumulative" ]]; then
+            cumulative="${core_pairs[$i]}"
+        else
+            cumulative="${cumulative},${core_pairs[$i]}"
+        fi
+        stress_sets[$i]="$cumulative"
     done
 
+    for load in low medium high; do
+        echo "$(tput setaf 4)Running ${load} load all core tests$(tput sgr0)" | tee -a "$output_log_file"
 
-    for core in $(seq $((total_cores - 2)) -1 0); do
-        cores_list=$(seq -s, 0 "$core")
-        cores_list=$(filter_cores "$cores_list")
-        [[ -z "$cores_list" ]] && echo "$(tput setaf 0)Skipping thread(s) $cores_list as disabled$(tput sgr0)" | tee -a "$output_log_file" && continue
-        echo "$(tput setaf 2)Stressing thread(s) [$cores_list] for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
-        update_threads "$cores_list"
-        start_stressor "$cores_list"
-        sleep "$all_core_time"
-        stop_stressor
-        check_errors
-    done
+        run_step() {
+            local threads="$1"
+            local filtered
+            filtered=$(filter_cores "$threads")
 
-    for core in $(seq $((total_cores - 1)) -1 0); do
-        cores_list=$(seq -s, "$core" $((total_cores - 1)))
-        cores_list=$(filter_cores "$cores_list")
-        [[ -z "$cores_list" ]] && echo "$(tput setaf 0)Skipping thread(s) $cores_list as disabled$(tput sgr0)" | tee -a "$output_log_file" && continue
-        echo "$(tput setaf 2)Stressing thread(s) [$cores_list] for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
-        update_threads "$cores_list"
-        start_stressor "$cores_list"
-        sleep "$all_core_time"
-        stop_stressor
+            if [[ -z "$filtered" ]]; then
+                echo "$(tput setaf 0)Skipping thread(s) $threads as disabled$(tput sgr0)" | tee -a "$output_log_file"
+                return
+            fi
+
+            echo "$(tput setaf 2)Stressing thread(s) [$filtered] with ${load} load for $all_core_time seconds$(tput sgr0)" | tee -a "$output_log_file"
+
+            update_threads "$filtered"
+            start_stressor "$filtered" "$load"
+            sleep "$all_core_time"
+            stop_stressor
+            check_errors
+        }
+
+        # Forward
+        for (( i=0; i<physical_cores; i++ )); do
+            run_step "${stress_sets[$i]}"
+        done
+
+        # Reverse
+        for (( i=physical_cores-1; i>=0; i-- )); do
+            run_step "${stress_sets[$i]}"
+        done
+
+        # Middle-out
+        local mid=$(( (physical_cores - 1) / 2 ))
+        local mo_cumulative="${core_pairs[$mid]}"
+        local -a mo_sets=("$mo_cumulative")
+
+        local lo=$(( mid - 1 ))
+        local hi=$(( mid + 1 ))
+
+        while (( lo >= 0 || hi < physical_cores )); do
+            if (( hi < physical_cores )); then
+                mo_cumulative="${core_pairs[$hi]},$mo_cumulative"
+            fi
+            if (( lo >= 0 )); then
+                mo_cumulative="${core_pairs[$lo]},$mo_cumulative"
+            fi
+            mo_sets+=("$mo_cumulative")
+            (( lo-- ))
+            (( hi++ ))
+        done
+
+        for set in "${mo_sets[@]}"; do
+            run_step "$set"
+        done
+
+        # Outside-in
+        local oi_cumulative="${core_pairs[0]}"
+        local -a oi_sets=("$oi_cumulative")
+
+        local oi_lo=$(( physical_cores - 1 ))
+        local oi_hi=1
+
+        while (( oi_hi <= oi_lo )); do
+            oi_cumulative="${oi_cumulative},${core_pairs[$oi_lo]}"
+            if (( oi_hi <= oi_lo )); then
+                oi_cumulative="${oi_cumulative},${core_pairs[$oi_hi]}"
+            fi
+            oi_sets+=("$oi_cumulative")
+            (( oi_lo-- ))
+            (( oi_hi++ ))
+        done
+
+        for set in "${oi_sets[@]}"; do
+            run_step "$set"
+        done
+
     done
 }
