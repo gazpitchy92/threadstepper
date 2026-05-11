97fe47214e65e09a45999c6d6c7b556a9aa8936e
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-11 05:03:38 +0100

    moving html files

--- functions/tests/single/browser.sh
+++ functions/tests/single/browser.sh
@@ -3,11 +3,11 @@ browser_pids=()
 
 browser_test() {
     echo "$(tput setaf 4)Launching $browsers browsers on all cores$(tput sgr0)" | tee -a $output_log_file
-    echo "$(tput setaf 8)[DEBUG] Using electron: $(which $ELECTRON_BIN)"
+    echo "$(tput setaf 8)[DEBUG] Using electron: $(which $electon_bin)"
     # For each webgl instance
     for ((i = 0; i < browsers; i++)); do
         random_page=$((RANDOM % 10 + 1))
-        file_path="$current_dir/tests/browser/pages/$random_page.html"
+        file_path="$current_dir/functions/tests/html/$random_page.html"
         num_cores=$(nproc)
         half_cores=$((num_cores / 2))
         if (( browsers > 1 )); then
@@ -15,19 +15,19 @@ browser_test() {
             if (( i % 2 == 0 )); then
                 echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list 0-$((half_cores - 1)) $file_path$(tput sgr0)" | tee -a $output_log_file
                 update_threads "0-$((half_cores - 1))"
-                taskset --cpu-list 0-$((half_cores - 1)) "$ELECTRON_BIN" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
+                taskset --cpu-list 0-$((half_cores - 1)) "$electon_bin" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
                 browser_pids+=($!)
             else
                 echo "$(tput setaf 3)[DEBUG Browser $((i+1))] taskset --cpu-list $half_cores-$((num_cores - 1)) $file_path$(tput sgr0)" | tee -a $output_log_file
                 update_threads "$half_cores-$((num_cores - 1))"
-                taskset --cpu-list $half_cores-$((num_cores - 1)) "$ELECTRON_BIN" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
+                taskset --cpu-list $half_cores-$((num_cores - 1)) "$electon_bin" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
                 browser_pids+=($!)
             fi
         else
             # All core
             echo "$(tput setaf 8)[DEBUG] taskset --cpu-list 0-$((num_cores - 1)) $file_path$(tput sgr0)" | tee -a $output_log_file
             update_threads "0-$((num_cores - 1))"
-            taskset --cpu-list 0-$((num_cores - 1)) "$ELECTRON_BIN" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
+            taskset --cpu-list 0-$((num_cores - 1)) "$electon_bin" "$current_dir/tests/browser/launch.js" "$file_path" > /dev/null 2>&1 &
             browser_pids+=($!)
         fi
         rest
