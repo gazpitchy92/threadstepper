a80111c9e52c76ba34529d6436b69b29cf1deceb
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2026-05-11 06:20:17 +0100

    Fix IFS

--- functions/errors.sh
+++ functions/errors.sh
@@ -6,7 +6,7 @@ check_errors() {
     first_line=$(head -n 1 "$error_log")
     if [ "$first_line" != "false" ]; then
         echo "$(tput setaf 1)ERROR FOUND - STOPPING TEST!$(tput sgr0)" | tee -a $output_log_file
-        tail -n +2 "$error_log" | while ifs= read -r line; do
+        tail -n +2 "$error_log" | while IFS= read -r line; do
             echo "$(tput setaf 1)$line$(tput sgr0)" | tee -a $output_log_file
         done
         cleanup
